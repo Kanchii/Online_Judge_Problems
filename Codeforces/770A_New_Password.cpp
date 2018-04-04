c5e3ca069ea1eac8c29c7bcf1a85731162ee82a0
Weiss <weisslipe@gmail.com>
2018-04-04 00:51:55 -0300

    Update

---
+++ Codeforces/770A_New_Password.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define INF 0x3f3f3f3f
+#define max(a,b) ((a) > (b) ? (a) : (b))
+#define min(a,b) ((a) < (b) ? (a) : (b))
+#define MAXN 100005
+#define MOD 1000000007
+#define pb push_back
+#define pu push
+#define DEBUG(x) cout << #x << " = " << x << endl
+#define fst first
+#define snd second
+
+typedef pair<int,int> ii;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+typedef vector<vi> vvi;
+typedef vector<vector<ii> > vvii;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+int main(int argc, char const *argv[]){
+    ios_base::sync_with_stdio(false);
+
+    string alpha = "abcdefghijklmnopqrstuvwxyz";
+
+    int n, k;
+    cin >> n >> k;
+
+    string res = "";
+    for(int i = 0; i < n; i++){
+        res += alpha[i % k];
+    }
+    cout << res << endl;
+
+    return 0;
+}
